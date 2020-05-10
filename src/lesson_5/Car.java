2b14d883edf3262180a1ecd22a092d43c1e55c4d
Roman Zheludkov <romzhel@mail.ru>
2020-05-10 16:42:49 +0300

    Lesson_5

--- src/lesson_5/Car.java
+++ src/lesson_5/Car.java
@@ -1,11 +1,17 @@
 package lesson_5;
 
-public class Car implements Runnable {
-    private static int CARS_COUNT;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.ReentrantLock;
 
-    static {
-        CARS_COUNT = 0;
-    }
+public class Car implements Runnable {
+    private static int CAR_INDEX;
+    private static ReentrantLock racePreparing = new ReentrantLock();
+    private static CyclicBarrier startAllowing = new CyclicBarrier(MainClass.CARS_COUNT);
+    private static ReentrantLock raceStarted = new ReentrantLock();
+    private static ReentrantLock raceWon = new ReentrantLock();
+    private static CountDownLatch finishCounter = new CountDownLatch(MainClass.CARS_COUNT);
+    private static ReentrantLock raceFinished = new ReentrantLock();
 
     private Race race;
     private int speed;
@@ -14,8 +20,8 @@ public class Car implements Runnable {
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
-        CARS_COUNT++;
-        this.name = "Участник #" + CARS_COUNT;
+        CAR_INDEX++;
+        this.name = "Участник #" + CAR_INDEX;
     }
 
     public String getName() {
@@ -28,15 +34,41 @@ public class Car implements Runnable {
 
     @Override
     public void run() {
+        if (racePreparing.tryLock()) {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        }
+
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            startAllowing.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        if (raceStarted.tryLock()) {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        }
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+
+        if (raceWon.tryLock()) {
+            System.out.println(this.name + " WIN");
+        }
+
+        finishCounter.countDown();
+
+        try {
+            finishCounter.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (raceFinished.tryLock()) {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
         }
     }
 }
